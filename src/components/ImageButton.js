f0d93e6394f46f2976297799012f229d87a872e8
luis felipe cardoso marinho <126625142+luisfelipecad@users.noreply.github.com>
2024-09-16 15:03:49 -0300

    teste

--- src/components/ImageButton.js
+++ src/components/ImageButton.js
@@ -14,22 +14,22 @@ function ImageButton() {
 
       {/* Ícones à esquerda */}
       <div className='d-flex align-items-center position-absolute start-0' style={{ paddingTop: '14px', paddingLeft: '115px' }}>
-        <a href="https://www.instagram.com" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/unitpac.afya/" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-instagram"></i> {/* Ícone do Instagram */}
         </a>
-        <a href="https://www.linkedin.com" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/school/unitpac/" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-linkedin"></i> {/* Ícone do LinkedIn */}
         </a>
-        <a href="https://www.youtube.com" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
+        <a href="https://www.youtube.com/unitpac" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-youtube"></i> {/* Ícone do YouTube */}
         </a>
-        <a href="https://www.facebook.com" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/Unitpac" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-facebook"></i> {/* Ícone do Facebook */}
         </a>
         <a href="https://wa.me/yourphonenumber" className='text-dark fs-3 me-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-whatsapp"></i> {/* Ícone do WhatsApp */}
         </a>
-        <a href="https://www.tiktok.com" className='text-dark fs-3' target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@unitpac" className='text-dark fs-3' target="_blank" rel="noopener noreferrer">
           <i className="bi bi-tiktok"></i> {/* Ícone do TikTok */}
         </a>
       </div>
