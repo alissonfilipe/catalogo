14ace784316d3359e26d90d817d66239293be593
luis felipe cardoso marinho <126625142+luisfelipecad@users.noreply.github.com>
2024-09-16 14:58:27 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
+import React from 'react';
 import Home from './pages/Home';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <>
+      <div className="App">
+        <Home />
+      </div>
+
+
+      <Footer />  {/* Footer no mesmo fragmento */}
+     
+      <div className="scrollable-div">
+        {/* imagem */}
+      </div>
+    </>
   );
 }
 
+
 export default App;
+
+
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,24 @@
 // Rodapé do site
+
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()}
+          <span className="footer-link">UNITPAC</span>
+          <span className="footer-separator"> | </span>
+          <span className="footer-link">IFTO</span>
+          <span className="footer-separator"> | </span>
+          <span className="footer-link">PORTAIS</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
