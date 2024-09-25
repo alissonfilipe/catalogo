d11b5cd9262383d20c11fe5c2e06774874b14a9c
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-09-25 17:10:08 -0300

    algumas configurações

--- src/components/ParteDetalhada.js
+++ src/components/ParteDetalhada.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 function ParteDetalhada() {
     const { nomeParte } = useParams();
 
     return (
         <div>
-            <h2>Detalhes sobre: {nomeParte}</h2>
+            <NavBar/>
+            <h2 className='text-center'>{nomeParte}</h2>
             {/* Aqui você pode adicionar mais informações ou lógica para mostrar detalhes */}
+            <Footer/>
         </div>
     );
 }
