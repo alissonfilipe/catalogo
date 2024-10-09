417886e12735f0da73b39b0658880ef89c14bc8f
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-10-09 16:39:02 -0300

    mudança de nome de arquivo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
 import Esqueletico from './pages/sistemas/Esqueletico'; // Importe o componente do sistema esquelético
-import ParteDetalhada from './components/ParteDetalhada'; // Importe o componente para os detalhes
+import Parte from './components/Parte'; // Importe o componente para os detalhes
 import NotFound from './pages/NotFound'; // Importe a página NotFound
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sistemas/esqueletico" element={<Esqueletico />} />
-          <Route path="/parte" element={<ParteDetalhada />} />
+          <Route path="/parte" element={<Parte />} />
           <Route path="*" element={<NotFound />} /> {/* Rota para Not Found */}
         </Routes>
       </div>
